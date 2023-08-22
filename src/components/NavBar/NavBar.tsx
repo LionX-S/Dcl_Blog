68323097daefab7e3fd943dad80c9b0480405c23
LionX-S <13141096848@163.com>
2023-08-22 22:38:32 +0800

    feat:新增NavBar组件样式

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,23 @@
-const NavBar = () => {
-  return (
-    <div className="bg-white">导航栏</div>
-  )
-}
-export default NavBar
+import { ReactNode } from "react";
+
+type NavBarProps = {
+	left: ReactNode;
+	middle: ReactNode;
+	right: ReactNode;
+};
+const NavBar = ({
+	left = "left",
+	middle = "middle",
+	right = "center"
+}: NavBarProps) => {
+	return (
+		<div className='box-border h-15 max-w-full min-w-full p-1 border-b border-solid border-slate-200 bg-slate-50/30 '>
+			<div className='container mx-auto h-full flex text-center'>
+				<div className="flex-none h-full leading-13">{left}</div>
+				<div className='flex-auto h-full leading-13 w-6/12'>{middle}</div>
+				<div className='flex-none h-full leading-13'>{right}</div>
+			</div>
+		</div>
+	);
+};
+export default NavBar;
