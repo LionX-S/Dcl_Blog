7b92800a3a17376cc748770fa4cbfc0ffad31854
LionX-S <13141096848@163.com>
2023-08-23 22:32:11 +0800

    feat:实现响应式NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 type NavBarProps = {
 	left: ReactNode;
@@ -8,16 +8,32 @@ type NavBarProps = {
 const NavBar = ({
 	left = "left",
 	middle = "middle",
-	right = "center"
+	right = "right"
 }: NavBarProps) => {
+	const [showSlide, setShowSlide] = useState<boolean>(false);
 	return (
-		<div className='box-border h-15 max-w-full min-w-full p-1 border-b border-solid border-slate-200 bg-slate-50/30 '>
-			<div className='container mx-auto h-full flex text-center'>
-				<div className="flex-none h-full leading-13">{left}</div>
-				<div className='flex-auto h-full leading-13 w-6/12'>{middle}</div>
-				<div className='flex-none h-full leading-13'>{right}</div>
+		<>
+			<div className='box-border h-15 max-w-full min-w-full p-1 border-b border-solid border-slate-200 bg-slate-50/30 '>
+				<div className='container mx-auto h-full flex text-center'>
+					<div className='flex-none h-full leading-13'>
+						<div className='hidden md:block'>{left}</div>
+						<div
+							className='md:hidden'
+							onClick={() => {
+								setShowSlide(true);
+							}}>
+							三
+						</div>
+					</div>
+					<div className='flex-auto h-full leading-13 w-6/12'>
+						<div className='hidden md:block'>{middle}</div>
+					</div>
+					<div className='flex-none h-full leading-13'>{right}</div>
+				</div>
 			</div>
-		</div>
+      {/* TODO 这里是否写成穿梭框更好些 */}
+			{showSlide && <div className='h-screen'>侧边栏</div>}
+		</>
 	);
 };
 export default NavBar;
